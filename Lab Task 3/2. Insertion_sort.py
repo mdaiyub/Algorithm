345b74b52463ebfa0dc1ec4dbe544698525187fa
Md. Aiyub Ali <aiyubali15-13456@diu.edu.bd>
2021-11-04 14:14:28 +0600

    Add files via upload

---
+++ Lab Task 3/2. Insertion_sort.py
@@ -0,0 +1,18 @@
+def insertionSort(array):
+
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+
+        array[j + 1] = key
+
+data = list(map(int, input().split()))
+insertionSort(data)
+print('Sorted Array in Ascending Order:')
+print(data)
+
+
